29fb5c7ece5dd5ad90aefebd648791e93ce1e1b2
Rishikesh Kumar <Rishikesh.Kumar@COFORGETECH.COM>
2021-07-09 15:55:38 +0530

    Added comments and removed console log code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,39 +16,40 @@
 
     <div class="home-view-container__row">
       <p class="home-view-container__row-left">Departure<span class="required-message">*</span>: </p>
-    <p class="home-view-container__row-right"><input type="date" name="searchOptions.departureDate" v-model="searchOptions.departureDate"/></p>
+      <p class="home-view-container__row-right"><input type="date" name="searchOptions.departureDate" v-model="searchOptions.departureDate"/></p>
     </div>
 
     <div class="home-view-container__row" v-if="searchOptions.isReturnFlightNeeded">
-    <p class="home-view-container__row-left">Return<span class="required-message">*</span>:</p>
-    <p class="home-view-container__row-right"><input type="date" name="searchOptions.returnDate" v-model="searchOptions.returnDate"/></p>
+      <p class="home-view-container__row-left">Return<span class="required-message">*</span>:</p>
+      <p class="home-view-container__row-right"><input type="date" name="searchOptions.returnDate" v-model="searchOptions.returnDate"/></p>
     </div>
 
     <div class="home-view-container__row">
-    <p class="home-view-container__row-left">Source<span class="required-message">*</span>:</p>
-    <p class="home-view-container__row-right">
+      <p class="home-view-container__row-left">Source<span class="required-message">*</span>:</p>
+      <p class="home-view-container__row-right">
       <select name="source" @change="onSourceChange($event)">
           <option>--- Select Source ---</option>
           <option v-for="item in airportNames" :value="item" :key="item">{{item}}</option>
       </select>
-    </p>
+      </p>
     </div>
 
     <div class="home-view-container__row">
-    <p class="home-view-container__row-left">Destination<span class="required-message">*</span>:</p>
-    <p class="home-view-container__row-right">
-      <select name="destination" @change="onDestChange($event)">
-          <option>--- Select destination ---</option>
-          <option v-for="item in airportNames" :value="item" :key="item">{{item}}</option>
-      </select>
-    </p>
+      <p class="home-view-container__row-left">Destination<span class="required-message">*</span>:</p>
+      <p class="home-view-container__row-right">
+        <select name="destination" @change="onDestChange($event)">
+            <option>--- Select destination ---</option>
+            <option v-for="item in airportNames" :value="item" :key="item">{{item}}</option>
+        </select>
+      </p>
     </div>
+
     <div class="home-view-container__row">
       <p class="home-view-container__row-left"><span class="passenger-count-text">Number of Passengers</span><span class="required-message">*</span>: <span class="passenger-total__count">{{searchOptions.passengersCount.total}}</span></p>
 
     </div>
     <div class="home-view-container__passenger">
-       <div class="home-view-container__passenger-adults">
+      <div class="home-view-container__passenger-adults">
       <div>Adults</div>
       <div>
       <button @click="decrement('adult')">-</button>
@@ -68,12 +69,12 @@
 
     <div class="home-view-container__passenger-infant">
       <div>Infant</div>
-      <div>
-      <button @click="decrement('infant')">-</button>
-      <input :value="searchOptions.passengersCount.infant" disabled />
-      <button @click="increment('infant')">+</button>
+        <div>
+          <button @click="decrement('infant')">-</button>
+          <input :value="searchOptions.passengersCount.infant" disabled />
+          <button @click="increment('infant')">+</button>
+        </div>
       </div>
-    </div>
     </div>
     <div class="home-view-container__error-message" v-if="isError">Please select all required fields..</div>
     <button class="search-btn" @click="showResult">Search</button>
@@ -110,18 +111,18 @@ export default {
   components: {
   },
   methods: {
+    // event to handle switch toggle for one way and return flight
     change (event) {
       const optionValue = event.target.value
       this.searchOptions.isReturnFlightNeeded = optionValue === '2'
-      console.log(event)
     },
     onSourceChange (event) {
       this.searchOptions.selectedSource = event.target.value
-      console.log(this.searchOptions.selectedSource)
     },
     onDestChange (event) {
       this.searchOptions.selectedDestination = event.target.value
     },
+    // Populating airport names and code
     populateAirportNames (airports) {
       airports.forEach((airportItem) => {
         this.airportNames.push(airportItem.name + ' (' + airportItem.code + ')')
@@ -131,6 +132,7 @@ export default {
       })
       this.airportNames = [...new Set(this.airportNames)]
     },
+    // applying initial validation for required fields
     checkValidation () {
       if (this.searchOptions.selectedSource != null && !!this.searchOptions.selectedDestination &&
       this.searchOptions.departureDate != null && this.searchOptions.passengersCount.total > 0) {
@@ -144,6 +146,7 @@ export default {
       }
       return false
     },
+    // This method is called on Search button click
     showResult () {
       this.formValidated = this.checkValidation()
       if (this.formValidated) {
@@ -153,6 +156,7 @@ export default {
         this.isError = true
       }
     },
+    // This method is meant to decrement passenger count
     decrement (passengerType) {
       if (passengerType === 'adult' && this.searchOptions.passengersCount.adults > 0) {
         this.searchOptions.passengersCount.adults--
@@ -165,6 +169,7 @@ export default {
       }
       this.calculatePssengersCount()
     },
+    // This method is meant to increment passenger count
     increment (passengerType) {
       if (passengerType === 'adult') {
         this.searchOptions.passengersCount.adults++
@@ -177,11 +182,13 @@ export default {
       }
       this.calculatePssengersCount()
     },
+    // Calculating total number of passengers including adults, children and infants
     calculatePssengersCount () {
       this.searchOptions.passengersCount.total = this.searchOptions.passengersCount.infant +
       this.searchOptions.passengersCount.children + this.searchOptions.passengersCount.adults
     }
   },
+  // Making Get request call to get airport names on Created hook method
   created () {
     FlightApi.getAirports().then((response) => {
       this.populateAirportNames(response)
